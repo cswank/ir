b9998ef1e56757ce2f6082ec0885283deaa36b5b
Craig Swank <craigswank@gmail.com>
2025-12-20 08:04:18 -0700

    Rename IR to NEC

--- src/root.zig
+++ src/root.zig
@@ -15,12 +15,12 @@ const frames: []const []const u64 = &[_][]const u64{
     &[_]u64{ 563, 1688 },
 };
 
-pub const IR = struct {
+pub const NEC = struct {
     val: u64 = 0,
     i: u6 = 0,
     j: u3 = 0,
 
-    pub fn put(self: *IR, duration: u64) bool {
+    pub fn put(self: *NEC, duration: u64) bool {
         const close = closeTo(duration, frames[self.j]);
         if (close.success) {
             if (self.j == 3) {
@@ -43,7 +43,7 @@ pub const IR = struct {
         return self.i == 32;
     }
 
-    pub fn value(self: *IR) !message {
+    pub fn value(self: *NEC) !message {
         const addr: u8 = @truncate(self.val);
         const iaddr: u8 = @truncate(self.val >> 8);
         const cmd: u8 = @truncate(self.val >> 16);
@@ -62,7 +62,7 @@ pub const IR = struct {
         return message{ .address = addr, .command = cmd };
     }
 
-    fn reset(self: *IR) void {
+    fn reset(self: *NEC) void {
         self.val = 0;
         self.i = 0;
         self.j = 0;
@@ -80,7 +80,7 @@ fn closeTo(d: u64, vals: []const u64) struct { success: bool, i: usize } {
 
 test "usage" {
     const pulses = [_]u64{ 39631, 9038, 4481, 579, 1689, 580, 554, 582, 1686, 581, 554, 582, 1686, 580, 1689, 579, 556, 579, 554, 580, 555, 577, 1691, 579, 555, 579, 1689, 580, 554, 580, 555, 579, 1690, 579, 1690, 579, 555, 579, 557, 578, 557, 577, 557, 554, 580, 578, 557, 578, 1689, 579, 556, 579, 1689, 578, 1690, 580, 1689, 578, 1689, 580, 1688, 579, 1690, 577, 558, 577, 1692, 554, 39631 };
-    var ir = IR{};
+    var ir = NEC{};
     for (pulses, 0..) |_, i| {
         if (ir.put(pulses[i])) {
             break;
